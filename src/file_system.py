38f87df41b69f0b9d38900e7991f2ac407c9cf33
unknown <arthurcapacitacion@gmail.com>
2020-11-13 12:48:59 -0400

    adding messages to file_system.py

--- src/file_system.py
+++ src/file_system.py
@@ -10,23 +10,26 @@ rnc_file_path = os.path.join(ROOT_DIR, 'files/TMP/DGII_RNC.TXT')
 
 
 def download_file():
+    print("downloading file...")
     url = "https://dgii.gov.do/app/WebApps/Consultas/RNC/DGII_RNC.zip"
     urlretrieve(url, zip_path)
 
 
 def unzip_file():
+    print("unzping file...")
     with ZipFile(zip_path, 'r') as zip_ref:
         zip_ref.extractall(ROOT_DIR + '/files')
 
 
 def importDataToDb():
+    print("reading file...")
     df = pd.read_csv(content, sep='|', encoding='utf-8', header=None)
     cols = [4,5,6,7]
     df.drop(df.columns[cols], axis=1, inplace=True)
     df.columns = ['rnc_cedula', 'business_name', 'comercial_name', 'service_type', 'registered_date', 'state', 'payment_scheme']
 
     conn = create_connection()
-
+    print("importing file to db...")
     df.to_sql('taxpayers', conn, if_exists='replace', index=False)
 
 
