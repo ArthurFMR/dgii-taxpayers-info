e1c181bb8533a62182d45ff0a94b11e180d87607
unknown <arthurcapacitacion@gmail.com>
2020-11-13 12:31:01 -0400

    Change enconding ansi to utf-8

--- src/file_system.py
+++ src/file_system.py
@@ -20,7 +20,7 @@ def unzip_file():
 
 
 def importDataToDb():
-    df = pd.read_csv(rnc_file_path, sep='|', encoding='ANSI', header=None)
+    df = pd.read_csv(content, sep='|', encoding='utf-8', header=None)
     cols = [4,5,6,7]
     df.drop(df.columns[cols], axis=1, inplace=True)
     df.columns = ['rnc_cedula', 'business_name', 'comercial_name', 'service_type', 'registered_date', 'state', 'payment_scheme']
